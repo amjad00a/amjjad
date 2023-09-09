7dc60fe5825d6700ee29a25900f9d8f52f749db0
amjad0112 <amja83d@gmail.com>
2023-09-09 18:43:37 +0300

    proo1

---
+++ Q1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+image=plt.imread("123.jpg")
+print(type(image))
+plt.subplot(121)
+plt.title("image1")
+plt.imshow(image)
+image2=plt.imread("456.jpg")
+plt.subplot(122)
+plt.imshow(image2)
+plt.title("image2")
+plt.axis("off")
+plt.show()
---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+image=plt.imread("123.jpg")
+plt.figure(figsize=(10,10))
+for i in range(20):
+    plt.subplot(10,2,i+1)
+    plt.title("image1")
+    plt.axis("off")
+    plt.imshow(image)
